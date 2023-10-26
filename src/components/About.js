9d8c7209459645dcf8ecd3526bef5eb5d142f5f0
Isaac <govegeku00@gmail.com>
2023-10-26 12:37:27 +0300

    Completed Lab

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,15 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>What a time to be alive!</p>
+    <img
+    className="image"
+    src="https://i.imgur.com/mV8PQxj.gif" 
+    alt="I made this"
+    ></img>
+  </div>;
 }
 
 export default About;
